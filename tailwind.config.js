20a4e77cdf2d45d8182488caba29976f9130a27d
Sergio Augusto Moreira Bastos Junior <sergiobastosfisio@yahoo.com.br>
2023-09-04 18:23:29 -0300

    responsividade

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      screens: {
+        1536: { max: "1536px", min: "1441px" },
+        1440: { max: "1440px", min: "1367px" },
+        1366: { min: "1366px", max: "1439px" },
+        md: "850px",
+      },
       fontFamily: {
         main: ["Poppins"],
       },
